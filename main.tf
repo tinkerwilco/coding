9928e5d59a7b514fcb042ac8a6b68cb388bb9ddf
tinkerwilco <light.hope8457@fastmail.com>
2022-09-19 15:45:18 -0700

    terraform fmt applied

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 variable "awsprops" {
-    type = "map"
-    default = {
-    region = "us-east-1"
-    vpc = "vpc-5234832d"
-    ami = "ami-0c1bea58988a989155"
-    itype = "t2.micro"
-    subnet = "subnet-81896c8e"
-    publicip = true
-    keyname = "myseckey"
+  type = map(string)
+  default = {
+    region       = "us-east-1"
+    vpc          = "vpc-5234832d"
+    ami          = "ami-0c1bea58988a989155"
+    itype        = "t2.micro"
+    subnet       = "subnet-81896c8e"
+    publicip     = true
+    keyname      = "myseckey"
     secgroupname = "IAC-Sec-Group"
   }
 }
@@ -17,31 +17,31 @@ provider "aws" {
 }
 
 resource "aws_security_group" "project-iac-sg" {
-  name = lookup(var.awsprops, "secgroupname")
+  name        = lookup(var.awsprops, "secgroupname")
   description = lookup(var.awsprops, "secgroupname")
-  vpc_id = lookup(var.awsprops, "vpc")
+  vpc_id      = lookup(var.awsprops, "vpc")
 
   // To Allow SSH Transport
   ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   // To Allow Port 80 Transport
   ingress {
-    from_port = 80
-    protocol = ""
-    to_port = 80
+    from_port   = 80
+    protocol    = ""
+    to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   lifecycle {
@@ -51,11 +51,11 @@ resource "aws_security_group" "project-iac-sg" {
 
 
 resource "aws_instance" "project-iac" {
-  ami = lookup(var.awsprops, "ami")
-  instance_type = lookup(var.awsprops, "itype")
-  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  ami                         = lookup(var.awsprops, "ami")
+  instance_type               = lookup(var.awsprops, "itype")
+  subnet_id                   = lookup(var.awsprops, "subnet") #FFXsubnet2
   associate_public_ip_address = lookup(var.awsprops, "publicip")
-  key_name = lookup(var.awsprops, "keyname")
+  key_name                    = lookup(var.awsprops, "keyname")
 
 
   vpc_security_group_ids = [
@@ -63,18 +63,18 @@ resource "aws_instance" "project-iac" {
   ]
   root_block_device {
     delete_on_termination = true
-    iops = 150
-    volume_size = 50
-    volume_type = "gp2"
+    iops                  = 150
+    volume_size           = 50
+    volume_type           = "gp2"
   }
   tags = {
-    Name ="SERVER01"
+    Name        = "SERVER01"
     Environment = "DEV"
-    OS = "UBUNTU"
-    Managed = "IAC"
+    OS          = "UBUNTU"
+    Managed     = "IAC"
   }
 
-  depends_on = [ aws_security_group.project-iac-sg ]
+  depends_on = [aws_security_group.project-iac-sg]
 }
 
 
